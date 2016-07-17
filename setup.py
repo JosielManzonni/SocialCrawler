f6bca95b26307490da432388f444b9d678072108
Wirlino <josiel.wirlino@gmail.com>
2016-07-17 13:00:39 -0400

    Add install requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# (c) J.Wirlino
+# Date July 15th 2016
+# https://github.com/JosielWirlino/TwitterSwarm4Square-DataMining
+
 from setuptools import setup
 
-setup( name='TwitterSwarm4Square-DataMining',
+setup( name='SocialCrawler',
 	version='0.1',
 	description='This package allow get data from Twitter, Swarm and Foursquare',
 	url='http://github.com/JosielWirlino/TwitterSwarm4Square-DataMining',
 	author='J.Wirlino',
 	author_email='josiel.wirlino@gmail.com',
 	license='GNU',
-	packages=['TwitterSwarm4Square-DataMining'],
-	install_requires=['tweepy',],
+	packages=['SocialCrawler'],
+	install_requires=['tweepy','requests'],
 	zip_safe=False
 	)
