e1fb772ff630521d43d83017e5a8a420f00a3e9e
Wirlino <josiel.wirlino@gmail.com>
2016-07-18 12:07:08 -0400

    added import ExtractorData

--- SocialCrawler/__init__.py
+++ SocialCrawler/__init__.py
@@ -4,6 +4,7 @@
 # Date July 15th 2016
 # https://github.com/JosielWirlino/SocialCrawler
 from  . import HistoricalCollector
+from  . import ExtractorData
 import time
 
 __all__ = [	"HistoricalCollector" ]
