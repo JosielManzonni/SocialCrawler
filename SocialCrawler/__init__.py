3ff5c1dff657ae4e19bb423f532eea603ebcb6c9
Wirlino <josiel.wirlino@gmail.com>
2016-07-18 14:52:25 -0400

    Hidden Extractor File

--- SocialCrawler/__init__.py
+++ SocialCrawler/__init__.py
@@ -4,7 +4,7 @@
 # Date July 15th 2016
 # https://github.com/JosielWirlino/SocialCrawler
 from  . import HistoricalCollector
-from  . import ExtractorData
+# from  . import ExtractorData
 import time
 
 __all__ = [	"HistoricalCollector" ]
