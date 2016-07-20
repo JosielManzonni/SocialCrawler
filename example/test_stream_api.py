d2ef913e68e13730cdc6e904d65e03576ddab8ad
Josiel Wirlino <josiel.wirleno@yahoo.com.br>
2016-07-20 11:22:55 -0400

    Update test_stream_api.py

--- example/test_stream_api.py
+++ example/test_stream_api.py
@@ -1,9 +1,9 @@
 from stream_api import TwitterStreamAPI
 
-api_key = "udF1vZod9mIWADiwCS5GwMjNI"
-api_secret = "omR4g3tfFciQvgraNHnL4ur2cOo3x7viGdBypog5EdNwnK1NZh"
-access_token = "4717654877-x2rcMWisECAD0peOEvmFRANLQnKUMICdMvh1HCJ"
-access_secret = "pK7CBPJHFXgSKZNrg2ohibNWRQS0sNX0cbXIcSfcL6guB"
+api_key = "----"
+api_secret = "----"
+access_token = "-----"
+access_secret = "---"
 
 test = TwitterStreamAPI(api_key,api_secret,access_token,access_secret)
 track=['partiu','#partiu','Oi','Boa tarde']
