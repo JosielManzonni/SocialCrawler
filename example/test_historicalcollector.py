118d367c54d0113dbb706f6cee21f0e27bbcceca
J.Wirlino <josiel.wirlino@gmail.com>
2016-09-06 15:43:04 -0400

    removed keys comments

--- example/test_historicalcollector.py
+++ example/test_historicalcollector.py
@@ -1,44 +1,44 @@
 from SocialCrawler import HistoricalCollector
 
-api_key = "----"
-api_secret = "----"
-access_token = "---"
-access_secret = "---"
+api_key = "udF1vZod9mIWADiwCS5GwMjNI"
+api_secret = "omR4g3tfFciQvgraNHnL4ur2cOo3x7viGdBypog5EdNwnK1NZh"
+access_token = "4717654877-x2rcMWisECAD0peOEvmFRANLQnKUMICdMvh1HCJ"
+access_secret = "pK7CBPJHFXgSKZNrg2ohibNWRQS0sNX0cbXIcSfcL6guB"
 
 test = HistoricalCollector.Collector(api_key,api_secret, access_token, access_secret)
-since='2016-07-11'
-until='2016-07-18'
+since='2016-08-28'
+until='2016-09-05'
 # test.getStoredData('swarmapp.com',since,until,'Rio de Janeiro','-22.911233,-43.448334,100km','Log/')
 
 
 international_capitals_geocode_dict = {}
-international_capitals_geocode_dict.update({'Bandung': '-6.903449,107.643158,100km'}) # Indonesia
-international_capitals_geocode_dict.update({'Bangkok': '13.724601,100.633111,100km'}) # Thailand
-international_capitals_geocode_dict.update({'Barcelona': '41.39479,2.148768,100km'}) # Spain
-international_capitals_geocode_dict.update({'Buenos Aires': '-34.615824,-58.43332,100km'}) # Argentina
-international_capitals_geocode_dict.update({'Chicago': '41.833733,-87.732155,100km'}) # USA
-international_capitals_geocode_dict.update({'Istanbul': '41.005322,29.012179,100km'}) # Turkey
-international_capitals_geocode_dict.update({'Jakarta': '6.229746,106.829518,100km'}) # Indonesia
-international_capitals_geocode_dict.update({'Kuala Lumpur': '3.139003,101.686855,100km'}) # Malaysia
-international_capitals_geocode_dict.update({'Kuwait City': '29.376065,47.981867,100km'}) # Kuwait
-international_capitals_geocode_dict.update({'London': '51.528642,-0.101599,100km'}) # UK
+# international_capitals_geocode_dict.update({'Bandung': '-6.903449,107.643158,100km'}) # Indonesia
+# international_capitals_geocode_dict.update({'Bangkok': '13.724601,100.633111,100km'}) # Thailand
+# international_capitals_geocode_dict.update({'Barcelona': '41.39479,2.148768,100km'}) # Spain
+# international_capitals_geocode_dict.update({'Buenos Aires': '-34.615824,-58.43332,100km'}) # Argentina
+# international_capitals_geocode_dict.update({'Chicago': '41.833733,-87.732155,100km'}) # USA
+# international_capitals_geocode_dict.update({'Istanbul': '41.005322,29.012179,100km'}) # Turkey
+# international_capitals_geocode_dict.update({'Jakarta': '6.229746,106.829518,100km'}) # Indonesia
+# international_capitals_geocode_dict.update({'Kuala Lumpur': '3.139003,101.686855,100km'}) # Malaysia
+# international_capitals_geocode_dict.update({'Kuwait City': '29.376065,47.981867,100km'}) # Kuwait
+# international_capitals_geocode_dict.update({'London': '51.528642,-0.101599,100km'}) # UK
 international_capitals_geocode_dict.update({'Los Angeles': '34.020499,-118.411732,100km'}) # USA
-international_capitals_geocode_dict.update({'Madrid': '40.437954,-3.679537,100km'}) # Spain
-international_capitals_geocode_dict.update({'Manila': '14.598072,120.979651,100km'}) # Philippines
-international_capitals_geocode_dict.update({'Melbourne': '37.860283,145.079616,100km'}) # Australia
-international_capitals_geocode_dict.update({'Mexico City': '19.390734,-99.143613,100km'}) # Mexico
-international_capitals_geocode_dict.update({'Moscow': '55.749792,37.632495,100km'}) # Russia
+# international_capitals_geocode_dict.update({'Madrid': '40.437954,-3.679537,100km'}) # Spain
+# international_capitals_geocode_dict.update({'Manila': '14.598072,120.979651,100km'}) # Philippines
+# international_capitals_geocode_dict.update({'Melbourne': '37.860283,145.079616,100km'}) # Australia
+# international_capitals_geocode_dict.update({'Mexico City': '19.390734,-99.143613,100km'}) # Mexico
+# international_capitals_geocode_dict.update({'Moscow': '55.749792,37.632495,100km'}) # Russia
 international_capitals_geocode_dict.update({'New York': '40.70565,-73.978003,100km'}) # USA
-international_capitals_geocode_dict.update({'Osaka': '34.6784,135.49515,100km'}) # Japan
-international_capitals_geocode_dict.update({'Paris': '48.858859,2.347557,100km'}) # France
+# international_capitals_geocode_dict.update({'Osaka': '34.6784,135.49515,100km'}) # Japan
+# international_capitals_geocode_dict.update({'Paris': '48.858859,2.347557,100km'}) # France
 international_capitals_geocode_dict.update({'San Francisco': '37.7577,-122.4376,100km'}) # USA
-international_capitals_geocode_dict.update({'Santiago': '-33.472788,-70.629831,100km'}) # Chile
-international_capitals_geocode_dict.update({'Semarang': '-7.024775,110.418848,100km'}) # Indonesia
-international_capitals_geocode_dict.update({'Seoul': '37.5651,126.98955,100km'}) # Korea
-international_capitals_geocode_dict.update({'Singapore': '1.314731,103.847019,100km'}) # Singapore
-international_capitals_geocode_dict.update({'Surabaya': '-7.275619,112.711684,100km'}) # Indonesia
-international_capitals_geocode_dict.update({'Sidney': '33.796923,150.922433,100km'}) # Australia
-international_capitals_geocode_dict.update({'Tokyo': '35.673343,139.710388,100km'}) # Japan
+# international_capitals_geocode_dict.update({'Santiago': '-33.472788,-70.629831,100km'}) # Chile
+# international_capitals_geocode_dict.update({'Semarang': '-7.024775,110.418848,100km'}) # Indonesia
+# international_capitals_geocode_dict.update({'Seoul': '37.5651,126.98955,100km'}) # Korea
+# international_capitals_geocode_dict.update({'Singapore': '1.314731,103.847019,100km'}) # Singapore
+# international_capitals_geocode_dict.update({'Surabaya': '-7.275619,112.711684,100km'}) # Indonesia
+# international_capitals_geocode_dict.update({'Sidney': '33.796923,150.922433,100km'}) # Australia
+# international_capitals_geocode_dict.update({'Tokyo': '35.673343,139.710388,100km'}) # Japan
 
 # Brazilian capitals geocode list
 brazilian_capitals_geocode_dict = {}
@@ -79,7 +79,7 @@ for city in international_cities:
 						until,
 						city,
 						international_capitals_geocode_dict.get(city),
-						'Log/')
+						'Log/Iner')
 
 brazilian_capitals = brazilian_capitals_geocode_dict.keys()
 for city in brazilian_capitals:
@@ -88,7 +88,7 @@ for city in brazilian_capitals:
 						until,
 						city,
 						brazilian_capitals_geocode_dict.get(city),
-						'Log/')
+						'Log/Brasil')
 
 #track=['partiu','#partiu','Oi','Boa Noite','noite','night',]
 #test.getStreamData(track,'Log/')
