aa5daadf35f8b98272c54cdf35387971bd74c366
J.Wirlino <josiel.wirlino@gmail.com>
2016-09-11 18:36:12 -0400

    removed credentials

--- example/test_historicalcollector.py
+++ example/test_historicalcollector.py
@@ -1,9 +1,9 @@
 from SocialCrawler import HistoricalCollector
 
-api_key = "udF1vZod9mIWADiwCS5GwMjNI"
-api_secret = "omR4g3tfFciQvgraNHnL4ur2cOo3x7viGdBypog5EdNwnK1NZh"
-access_token = "4717654877-x2rcMWisECAD0peOEvmFRANLQnKUMICdMvh1HCJ"
-access_secret = "pK7CBPJHFXgSKZNrg2ohibNWRQS0sNX0cbXIcSfcL6guB"
+api_key = "--"
+api_secret = "--"
+access_token = "--"
+access_secret = "---"
 
 test = HistoricalCollector.Collector(api_key,api_secret, access_token, access_secret)
 since='2016-08-28'
