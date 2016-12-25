4efd6a30f9267cb7bb5cf986ae71f39c85c5209f
Wirlino <josiel.wirlino@gmail.com>
2016-12-24 20:00:44 -0400

    updated python version to compile

--- updatedis.sh
+++ updatedis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #register the package
-sudo python3.5 setup.py register
+sudo python3 setup.py register
 #publish a 'downloadble' version
-sudo python3.5 setup.py sdist upload
+sudo python3 setup.py sdist upload
